b93cda258495cb163d10d21e4e40cb43d26be0e8
ankitha pille <ankithapille2010@gmail.com>
2021-07-10 17:36:22 -0500

    07/10

--- FindReplaceString/Program.cs
+++ FindReplaceString/Program.cs
@@ -7,34 +7,33 @@ namespace FindReplaceString
     {
         static void Main(string[] args)
         {
-            string[] sources = { "bn", "zzc","wxc" };
-            string[] targets = { "t", "lwb", "nee" }; // "vbfrssozp"
-            int[] indices = { 5, 2,7 };
-            Console.WriteLine(FindReplaceString("wqzzcbnwxc",indices,sources,targets));
+           
+            string[] sources = { "hvf", "vzr", "cw", "jvo", "jo", "qb", "ar", "noqf", "dv", "rh", "ri", "wt", "mx", "gux", "dc", "eic", "kz", "ct", "kidn", "lq", "ud", "odll", "vc", "tm", "qz", "no", "om", "bn", "ahm", "vra", "jeqco", "ml", "xb" };
+            string[] targets = { "ajq", "zb", "r", "fai", "e", "zs", "io", "snxd", "nw", "oi", "ofb", "quq", "gj", "nsys", "dk", "sf", "muj", "ll", "hqx", "k", "n", "ptrya", "f", "qek", "u", "dhj", "e", "kr", "waj", "rvkr", "roaoeq", "mci", "djw" }; // "vbfrssozp"
+            int[] indices = { 1, 31, 44, 70, 23, 73, 76, 92, 90, 86, 42, 4, 50, 17, 53, 20, 55, 15, 38, 64, 25, 9, 7, 68, 60, 88, 96, 47, 57, 34, 81, 78, 28 };
+            Console.WriteLine(FindReplaceString("ehvfwtrvcodllgjctguxeicjoudmxbevzrvravkidnricwsbnxmxvdckzahmqzbrlqugtmjvoqbxarmlgjeqcorhnodvnoqfomdp", indices,sources,targets));
         }
 
         public static string FindReplaceString(string s, int[] indices, string[] sources, string[] targets)
         {
+                SortedDictionary<int, int> str = new SortedDictionary<int, int>(); 
+                 for(int i = 0; i < indices.Length; i++)
+                 {
+                     if (s.Substring(indices[i], sources[i].Length).Equals(sources[i]))
+                     {
+                         str.Add(indices[i],i);
+                     }
 
-            SortedDictionary<int, int> str = new SortedDictionary<int, int>(); 
-            for(int i = 0; i < indices.Length; i++)
-            {
-                if (s.Substring(indices[i], sources[i].Length).Equals(sources[i]))
-                {
-                    str.Add(indices[i],i);
-                }
-                
-            }
-            int offset = 0;
-            foreach(KeyValuePair<int,int> st in str)
-            {
-                Console.WriteLine(s.Substring(st.Key, sources[st.Value].Length + offset));
-
-                s = s.Replace(s.Substring(st.Key,sources[st.Value].Length+offset),targets[st.Value]);
-                Console.WriteLine(s);
-                offset = offset + targets[st.Value].Length - sources[st.Value].Length;
-            }
-            return s;
+                 }
+                 int offset = 0;
+                 Console.WriteLine(s);
+                 foreach(KeyValuePair<int,int> st in str)
+                 {
+                     int Place = st.Key + offset;
+                     s = s.Remove(Place, (sources[st.Value].Length)).Insert(Place, targets[st.Value]);
+                     offset = offset + targets[st.Value].Length - sources[st.Value].Length;
+                 }
+                return s;
         }
     }
 }
