8d1ac3fd0b3aafa1d2adfbd386444fe790ab9a68
ankitha pille <ankithapille2010@gmail.com>
2021-07-09 20:28:43 -0500

    07-09-2021

--- GooglePrep/Program.cs
+++ GooglePrep/Program.cs
@@ -8,9 +8,9 @@ namespace GooglePrep
     {
         static void Main(string[] args)
         {
-            int[] nums = { 0, 1, 3, 50, 75 };
-            int lower =0;
-            int upper = 99;
+            int[] nums = {-1};
+            int lower =-1;
+            int upper = -1;
             var result = FindMissingRanges(nums, lower, upper);
             foreach(string s in result)
             {
@@ -23,41 +23,34 @@ namespace GooglePrep
             int lowerIndex = lower;
             if (nums.Length == 0)
             {
-                if (lower != upper)
-                    Console.WriteLine(lower + "->" + upper);
+                if (lower != upper) 
+                    resultList.Add(lower + "->" + upper);
                 else
-                    Console.WriteLine(lower);
+                    resultList.Add(lower.ToString());
             }
-            else if (nums[nums.Length - 1] < lower || nums[0] > upper)
-            {
-                if (lower != upper)
-                    Console.WriteLine(lower + "->" + upper);
-                else
-                    Console.WriteLine(lower);
-            }
-
+          
             else
             {
+                if(nums[0]>lower)
+                    if(lower == nums[0]-1)
+                        resultList.Add(lower.ToString());
+                    else
+                    resultList.Add((lower) + "->" +( nums[0]-1));
                 for (int i = 0; i < nums.Length - 1; i++)
                 {
-                    if (nums[i] < lower)
-                        continue;
-                    if (nums[i] == lower)
-                        lowerIndex = lower + 1;
-                    Console.WriteLine(lowerIndex+" "+ nums[i]);
-                    if (nums[i + 1] - lowerIndex == 2)
-                    {
-                        Console.WriteLine(lowerIndex);
+                    if(nums[i+1]-nums[i]==2 && nums[i]+1>= lower && nums[i]+1<=upper)
+                        resultList.Add((nums[i]+1).ToString());
+
+                    if (nums[i + 1] - nums[i] > 2)
+                    {            
+                        resultList.Add((nums[i] + 1) + "->" + (nums[i + 1] - 1));     
                     }
-
-                    if (nums[i + 1] - lowerIndex > 2)
-                        Console.WriteLine(lowerIndex + "->" + (nums[i + 1] - 1).ToString());
-                    lowerIndex = nums[i]+1;
-
                 }
                 if (nums[nums.Length - 1] < upper)
-                    Console.WriteLine(lowerIndex + "->" + upper);
-                //Console.WriteLine(lowerIndex);
+                    if (nums[nums.Length - 1] + 1 == upper)
+                        resultList.Add(upper.ToString());
+                    else
+                        resultList.Add(nums[nums.Length - 1]+1 + "->" + upper);
             }
             return resultList; 
 
