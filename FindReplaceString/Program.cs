fed9312bdd13a83c77938cd92a2e7b875e7e0148
ankitha pille <ankithapille2010@gmail.com>
2021-07-09 22:32:33 -0500

    ghgh

---
+++ FindReplaceString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FindReplaceString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sources = { "kg", "ggq","mo" };
+            string[] targets = { "s", "so", "bfr" }; // "vbfrssozp"
+            int[] indices = { 3, 5,1 };
+            Console.WriteLine(FindReplaceString("vmokgggqzp",indices,sources,targets));
+        }
+
+        public static string FindReplaceString(string s, int[] indices, string[] sources, string[] targets)
+        {
+           
+            int offset = 0;
+            for(int i = 0; i < indices.Length; i++)
+            {
+                if (s.Substring(indices[i]+offset, sources[i].Length).Equals(sources[i]))
+                {
+                    s = s.Replace(sources[i], targets[i]);
+                    Console.WriteLine(s);
+                    offset = targets[i].Length-sources[i].Length;
+                }
+                else
+                    offset = 0;
+            }
+            return s;
+        }
+    }
+}
