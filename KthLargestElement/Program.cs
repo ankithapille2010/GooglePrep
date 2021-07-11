68ea65408e3408a8f4ee73837080975336d83aec
ankitha pille <ankithapille2010@gmail.com>
2021-07-11 17:19:16 -0500

    07/11

---
+++ KthLargestElement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KthLargestElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 1, 2, 5, 6, 4};
+            Console.WriteLine(FindKthLargest(nums,2));
+        }
+        public static int FindKthLargest(int[] nums, int k)
+        {
+            int KthLargest = 0;
+            SortedList<int, int> list = new SortedList<int, int>();
+            for(int i =0;i<nums.Length;i++)
+            {
+                if (list.ContainsKey(nums[i]))
+                    list[nums[i]]++;
+                else
+                    list.Add(nums[i], 1);
+            }
+            
+            if (list.Count != 0)
+            {
+                //Console.WriteLine(list.Keys[list.Count - 1]);
+                for(int i = 0; i < k-1; i++)
+                {
+                    if(list[list.Keys[list.Count - 1]]>1)
+                        list[list.Keys[list.Count - 1]]--;
+                    else 
+                        list.Remove(list.Keys[list.Count - 1]);
+                }
+                KthLargest=(list.Keys[list.Count - 1]);
+            }
+                //KthLargest = list.Keys[list.Count - k];
+            return KthLargest;
+        }
+
+    }
+}
---
+++ TrappingRainWater/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrappingRainWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = { 4, 2, 0, 3, 2, 5 };
+            Console.WriteLine(Trap(height));
+        }
+        public static int Trap(int[] height)
+        {
+            int waterUnits = 0;
+            if (height.Length == 0)
+                return waterUnits;
+            int[] left_max = new int[height.Length];
+            int[] right_max = new int[height.Length];
+
+            int leftM = height[0];
+            left_max[0]=(height[0]);
+            for (int i = 1; i < height.Length; i++)
+            {
+                //leftM = ;
+                left_max[i]=(Math.Max(left_max[i - 1], height[i]));
+            }
+            int rightM = height[height.Length - 1];
+            right_max[height.Length-1]=height[height.Length - 1];
+            for (int i = height.Length - 2; i >= 0; i--)
+            {
+                //rightM =;
+                right_max[i]=(Math.Max(right_max[i + 1], height[i]));
+            }
+
+            for (int i = 1; i < left_max.Length - 1; i++)
+            {
+                waterUnits = waterUnits + Math.Min(right_max[i], left_max[i]) - height[i];
+            }
+            return waterUnits;
+
+        }
+    }
+}
