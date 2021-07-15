5f7cf8e5e36d2319981611ec3a82db99b97f31fa
ankitha pille <ankithapille2010@gmail.com>
2021-07-14 20:24:23 -0500

    07/14

---
+++ KClosestPoint-Atl/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KClosestPoint_Atl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] input = { new int[] { 1, 3 }, new int[] { -2, -2 } };
+            int[][] result = KClosest(input, 2);
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                Console.WriteLine(result[i][0]);
+                Console.WriteLine(result[i][1]);
+
+            }
+        }
+        public static int[][] KClosest(int[][] points, int k)
+        {
+            int[][] result = new int[k][];
+            Array.Sort(points, (a,b)=>DistanceSquared(a).CompareTo(DistanceSquared(b)));
+            Array.Copy(points,result,k);
+            return result;
+        }
+
+        public static double DistanceSquared(int [] point)
+        {
+            double dist = point[0]*point[0]+ point[1]*point[1];
+            return dist;
+        }
+    }
+}
--- KClosestPoint/Program.cs
+++ KClosestPoint/Program.cs
@@ -3,36 +3,64 @@ using System.Collections.Generic;
 
 namespace KClosestPoint
 {
-    class Program
+    class Point
+    {
+        public int X;
+        public int Y;
+        public double DistaceFromZero;
+        public Point(int x, int y, double dist)
+        {
+            X = x;
+            Y = y;
+            DistaceFromZero=dist;
+        }
+        public Point()
+        {
+
+        }
+    }
+    class Program: Point
     {
         static void Main(string[] args)
         {
-            int[][] input = { new int[] { 1, 3 }, new int[] { -2, 2 }};
-            int[][] result = KClosest(input, 1);
+            int[][] input = { new int[] { 1, 0 }, new int[] { 0, 1}};
+            int[][] result = KClosest(input, 2);
             for (int i = 0; i < result.GetLength(0); i++)
             {
-             //   Console.WriteLine(result[i][0]);
-               // Console.WriteLine(result[i][1]);
+                Console.WriteLine(result[i][0]);
+                Console.WriteLine(result[i][1]);
 
             }
         }
 
-            public static int[][] KClosest(int[][] points, int k)
+
+        public static int[][] KClosest(int[][] points, int k)
         {
-            int[][] result = new int [k][];
-            SortedList<double, int> res = new SortedList<double, int>();
-            for (int i = 0; i < points.GetLength(0); i++)
-            {
-                res.Add(Math.Sqrt(Math.Pow(points[i][0],2)+ Math.Pow(points[i][1], 2)),i);
+            int[][] result = new int[k][];
+            SortedList<double, string> pointDist = new SortedList<double, string>();
+
+            for (int i = 0; i < points.GetLength(0); i++) { 
+                double currDist = Math.Sqrt(Math.Pow(points[i][0], 2) + Math.Pow(points[i][1], 2));
+                 if (pointDist.ContainsKey(currDist))
+                {
+                    pointDist[currDist] = pointDist[currDist] + "," + i;
+                }
+                else
+                pointDist.Add(currDist,i.ToString());
             }
-            foreach(KeyValuePair<double,int> key in res)
-            {
-                Console.WriteLine(key.Key + " "+ key.Value);
-            }
-            for(int i=0;i<k;i++)
-            {
-                
-                result[i] = new int[] { points[i][0], points[i][1]};
+
+          
+            int counter = 0;
+            for(int i =0;i<k;i++){
+                 foreach (string s in pointDist.Values[i].Split(','))
+                {
+                    result[counter]= new int [] { points[Convert.ToInt32(s)][0], points[Convert.ToInt32(s)][1] };
+                    counter++;
+                }
+                if (counter == k)
+                {
+                    break;
+                }
             }
             return result;
         }
