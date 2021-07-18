6f72b6d20c4f8557f7232a70e545d920c6d2c04a
ankitha pille <ankithapille2010@gmail.com>
2021-07-18 16:48:37 -0500

    07/18

---
+++ BinaryTreeMaxPathSum/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace BinaryTreeMaxPathSum
+{
+    public class TreeNode
+    {
+       public int? val;
+       public TreeNode left;
+       public TreeNode right;
+       public TreeNode(int? val = 0, TreeNode left = null, TreeNode right = null)
+       {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+       }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int?[] inputs = new int? []{-10, 9, 20, null, null, 15, 7 };
+
+            TreeNode root = new TreeNode(-10, null, null);
+            root.left = new TreeNode(9,null,null);
+            root.right = new TreeNode(20, null, null);
+            TreeNode currNode = root;
+            int i = 1;
+
+            while (i < inputs.Length)
+            {
+                Console.WriteLine(currNode.val);
+                currNode.left = new TreeNode(inputs[i]);
+                i++;
+                currNode.right = new TreeNode(inputs[i]);
+                i++;
+
+                if (currNode.left == null)
+                    currNode = currNode.left;
+                else
+                    currNode = currNode.right;
+            }
+         }
+
+        private static void ParseBinaryTree(TreeNode root)
+        {
+            while (root != null)
+            {
+                Console.WriteLine();
+            }
+
+        }
+
+        private static TreeNode InsertIntoBinaryTree(int?[] inputs)
+        {
+            TreeNode root = new TreeNode(inputs[0],null,null);
+            TreeNode currNode = root;
+            for(int i =1;i< inputs.Length-2; i++)
+            {
+               // Console.WriteLine()
+                currNode.left = new TreeNode(inputs[i], null, null);
+                currNode.right = new TreeNode(inputs[i + 1], null, null);
+                currNode = currNode.left!=null?currNode.left:currNode.right;
+            }
+            return root;
+        }
+    }
+}
--- CourseSchedule2/Program.cs
+++ CourseSchedule2/Program.cs
@@ -7,45 +7,114 @@ namespace CourseSchedule2
     {
         static void Main(string[] args)
         {
-            int numCourses = 4;
-            int[][] prerequisites = { new int[] { 1, 0 }, new int[] { 2, 0 }, new int[] { 3, 1 }, new int[] { 3, 2 } };
-            Console.WriteLine(FindOrder(numCourses, prerequisites));
+            int numCourses = 3;
+            int[][] prerequisites = { new int[] { 1, 0 }, new int[] { 1, 2 }, new int[] { 0,1 } };
+            int [] result = (FindOrder(numCourses, prerequisites));
+            foreach (int i in result)
+                Console.WriteLine(i);
         }
 
         public static int[] FindOrder(int numCourses, int[][] prerequisites)
         {
-            List<int> result = new List<int>();
             IDictionary<int, List<int>> adj = new Dictionary<int, List<int>>();
             for(int i= 0;i < prerequisites.GetLength(0); i++)
             {
                 if (adj.ContainsKey(prerequisites[i][0]))
                 {
-                    adj[i].Add(prerequisites[i][1]);
+                    adj[prerequisites[i][0]].Add(prerequisites[i][1]);
                 }
                 else
                     adj.Add(prerequisites[i][0],new List<int> { prerequisites[i][1] });
             }
+            for(int i = 0; i < numCourses; i++)
+            {
+                if (!adj.ContainsKey(i))
+                    adj.Add(i, new List<int> { });
+            }
 
-            //foreach(KeyValuePair<int, List<int>> k in adj)
-            //{
-            //    Console.Write("\n" + k.Key+ ":");
-            //        foreach (int v in k.Value)
-            //        {
-            //        Console.WriteLine(v + ",");
-            //        }
-            //}
-            TopologiocalSort(adj);
-            return result.ToArray();
+            List<int>result = TopologiocalSort(adj);
+            return result.Count==numCourses?result.ToArray():new int[] { };
 
         }
 
         public static List<int> TopologiocalSort(IDictionary<int, List<int>> adj)
         {
             List<int> topologicalOrder = new List<int>();
+            Queue<int> order = new Queue<int>();
+            bool[] visited = new bool[adj.Count];
+            for (int i = 0; i < adj.Count; i++) {
+                visited[i] = false;
+            }
+            foreach (KeyValuePair<int, List<int>> k in adj)
+            {
+                
+                if (k.Value.Count==0)
+                {
+                    order.Enqueue(k.Key);
+                    visited[k.Key] = true;
+                }
+            }
 
-         //   foreach()
+            while(order.Count != 0)
+            {
+                int currNode = order.Dequeue();
+                topologicalOrder.Add(currNode);
+                foreach(KeyValuePair<int,List<int>> a in adj)
+                {
+                    if (a.Value.Contains(currNode)&& visited[a.Key]== false)
+                    {
+                        a.Value.Remove(currNode);
+                        if (a.Value.Count == 0)
+                        {
+                            order.Enqueue(a.Key);
+                            visited[a.Key] = true;
+                        }
+                    }
+                }
+            }
             return topologicalOrder;
 
+        }
+
+        private static IList<int> DFS(int Node, IDictionary<int, List<int>> adj, List<int>visited)
+        {
+
+            /* if (!adj.ContainsKey(Node) || adj[Node].Count == 0)
+             {
+                 if(!visited.Contains(Node))
+                     visited.Add(Node);
+             }
+
+             else
+             {
+                 foreach (int adjnode in adj[Node])
+                 {
+                     DFS(adjnode, adj, visited);
+                 }
+             }
+            */
+            Queue<int> order = new Queue<int>();
+            order.Enqueue(Node);
+            while (order.Count != 0)
+            {
+                int n = order.Dequeue();
+               // Console.WriteLine(n);
+                if (!adj.ContainsKey(n) || adj[n].Count == 0)
+                {
+                    if (!visited.Contains(n))
+                        visited.Add(n);
+                }
+                else
+                {
+                    foreach (int adjnode in adj[n])
+                    {               
+                        order.Enqueue(adjnode);
+                    }
+                }
+               
+            }
+            return visited;
+            
         }
     }
 }
