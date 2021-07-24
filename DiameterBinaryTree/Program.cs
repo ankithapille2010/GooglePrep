7bd94e4a4cf7dc0bab940a0353daa56755f01a99
ankitha pille <ankithapille2010@gmail.com>
2021-07-24 15:00:34 -0500

    07/24

---
+++ DiameterBinaryTree/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DiameterBinaryTree
+{
+    public class TreeNode
+    {
+        public int? val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int? val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        public static int diameter = 0;
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1, null, null);
+            root.left = new TreeNode(2, null, null);
+            root.right = new TreeNode(3, null, null);
+
+            root.left.left = new TreeNode(4, null, null); ;
+            root.left.right = new TreeNode(5, null, null); ;
+
+            root.right.left = new TreeNode(6, null, null);
+            
+            Console.WriteLine(DiameterOfBinaryTree(root));
+        }
+        public static int DiameterOfBinaryTree(TreeNode root)
+        {
+            LongestPath(root);
+            return diameter;
+          
+        } 
+
+        private static int LongestPath(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            int leftPath = LongestPath(root.left);
+            int righPath = LongestPath(root.right);
+            diameter = Math.Max(diameter, leftPath + righPath);
+
+            return Math.Max(leftPath,righPath)+1;
+        }
+    }
+}
---
+++ IsomorphicStrings/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsomorphicStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsomorphicStrings("ab", "aa"));
+        }
+
+        private static bool IsomorphicStrings(string s, string t)
+        {
+            Dictionary<char, char> str = new Dictionary<char, char>();
+           // Dictionary<char, char> strR = new Dictionary<char, char>();
+            if (str.Count == 1)
+            {
+                str.Add(s[0], t[0]);
+                str.Add(t[0], s[0]);
+             //   strR.Add(t[0], s[0]);
+            }
+            
+            for(int i=1; i< s.Length; i++)
+            {
+                if (!str.ContainsKey(s[i]))
+                {
+                   
+                    if (strR.ContainsKey(t[i]))
+                        return false;
+                    else
+                    {
+                        str.Add(s[i], t[i]);
+                        strR.Add(t[i], s[i]);
+                    }
+                    
+                }
+                else {
+                    if (str[s[i]] != t[i])
+                        return false;
+
+                }
+            }
+            return true;
+        }
+
+        
+    }
+}
