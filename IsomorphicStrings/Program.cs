822591048e9cdb95e78a0127adb2b71d16cf6e9d
ankithapille <ankithapille2010@gmail.com>
2021-07-25 12:36:43 -0500

    * GooglePrep.sln:
    * Program.cs: dsfdf

--- IsomorphicStrings/Program.cs
+++ IsomorphicStrings/Program.cs
@@ -7,32 +7,25 @@ namespace IsomorphicStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsomorphicStrings("ab", "aa"));
+            Console.WriteLine(IsomorphicStrings("paper", "title"));
         }
 
         private static bool IsomorphicStrings(string s, string t)
         {
             Dictionary<char, char> str = new Dictionary<char, char>();
-           // Dictionary<char, char> strR = new Dictionary<char, char>();
-            if (str.Count == 1)
+            Dictionary<char, char> strV = new Dictionary<char, char>();
+            for (int i=0; i< s.Length; i++)
             {
-                str.Add(s[0], t[0]);
-                str.Add(t[0], s[0]);
-             //   strR.Add(t[0], s[0]);
-            }
-            
-            for(int i=1; i< s.Length; i++)
-            {
+
                 if (!str.ContainsKey(s[i]))
                 {
-                   
-                    if (strR.ContainsKey(t[i]))
-                        return false;
-                    else
+                    if (!strV.ContainsKey(t[i]))
                     {
                         str.Add(s[i], t[i]);
-                        strR.Add(t[i], s[i]);
+                        strV.Add(t[i], s[i]);
                     }
+                    else
+                        return false;
                     
                 }
                 else {
@@ -40,6 +33,8 @@ namespace IsomorphicStrings
                         return false;
 
                 }
+                
+                 
             }
             return true;
         }
