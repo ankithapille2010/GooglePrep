9e8c16ba81f787fb558b26515e6f72a4b5f3f365
ankithapille <ankithapille2010@gmail.com>
2021-07-25 12:36:26 -0500

    * Program.cs:
    * cowAndBulls.csproj: 07/25

---
+++ cowAndBulls/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cowAndBulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetHint("1122", "1222"));
+        }
+        public static string GetHint(string secret, string guess)
+        {
+            int numOfBulls = 0;
+            int numOfCows = 0;
+            Dictionary<char, SortedSet<int>> sect = new Dictionary<char, SortedSet<int>>();
+            Dictionary<char, SortedSet<int>> gues = new Dictionary<char, SortedSet<int>>();
+            for (int i=0;i<secret.Length;i++)
+            {
+                if (!sect.ContainsKey(secret[i]))
+                {
+                    
+                    sect.Add(secret[i], new SortedSet<int> { i });
+                }
+                else
+                    sect[secret[i]].Add(i);
+
+            }
+
+            for (int i = 0; i < guess.Length; i++)
+            {
+                if (!gues.ContainsKey(guess[i]))
+                {
+
+                    gues.Add(guess[i], new SortedSet<int> { i });
+                }
+                else
+                    gues[guess[i]].Add(i);
+
+            }
+
+            //First pass for bulls
+            foreach(var g in gues)
+            {
+                if (sect.ContainsKey(g.Key))
+                {
+
+                   foreach(int i in g.Value)
+                    {
+                        if (sect[g.Key].Contains(i))
+                        {
+                            if (sect[g.Key].Count > 1)
+                                sect[g.Key].Remove(i);
+                            else
+                                sect.Remove(g.Key);
+
+                            numOfBulls++;
+                        }
+                    }
+                }
+            }
+            //Second pass for cows
+            foreach (var g in gues)
+            {
+                if (sect.ContainsKey(g.Key))
+                {
+
+                    foreach (int i in g.Value)
+                    {
+                        //if (sect[g.Key].Contains(i))
+                        //{
+                            if (sect[g.Key].Count > 1)
+                                sect[g.Key].Remove(i);
+                            else
+                                sect.Remove(g.Key);
+
+                            numOfCows++;
+                        //}
+                    }
+                }
+            }
+            return new string(numOfBulls+"A"+numOfCows+"B");
+        }
+    }
+}
